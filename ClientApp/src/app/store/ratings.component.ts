ab86fb850d9a171b19598a45b35da8f8ab92d7b0
vkradio <37143087+vkradio@users.noreply.github.com>
2020-05-05 01:52:56 +0500

    Chapter 8 (finished): Ratings

--- ClientApp/src/app/store/ratings.component.ts
+++ ClientApp/src/app/store/ratings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Component({
@@ -6,5 +6,26 @@ import { Product } from '../models/product.model';
   templateUrl: 'ratings.component.html'
 })
 export class RatingsComponent {
+  @Input()
+  product: Product;
 
+  get stars(): boolean[] {
+    if (this.product != null && this.product.ratings != null) {
+      const total = this
+        .product
+        .ratings
+        .map(r => r.stars)
+        .reduce((prev, curr) => prev + curr, 0);
+
+      const count = Math.round(total / this.product.ratings.length);
+
+      return Array(5)
+        .fill(false)
+        .map((value, index) => {
+          return index < count;
+        });
+    } else {
+      return [];
+    }
+  }
 }
