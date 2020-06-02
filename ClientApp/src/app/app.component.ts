7c088e6c94c8387fa495345ec2bc256c7477a25b
vkradio <37143087+vkradio@users.noreply.github.com>
2020-06-02 23:43:55 +0500

    Chapter 11 - Step 2: Interception and displaying HTTP errors

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Repository } from './models/repository';
-import { Product } from './models/product.model';
-import { Supplier } from './models/supplier.model';
+import { ErrorHandlerService } from './errorHandler.service';
 
 @Component({
   selector: 'app-root',
@@ -9,65 +7,19 @@ import { Supplier } from './models/supplier.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private lastError: string[];
 
-  constructor(private repo: Repository) { }
-
-  get product(): Product {
-    return this.repo.product;
+  constructor(errorService: ErrorHandlerService) {
+    errorService.errors.subscribe(error => {
+      this.lastError = error;
+    });
   }
 
-  get products(): Product[] {
-    return this.repo.products;
+  get error() {
+    return this.lastError;
   }
 
-  createProduct() {
-    this.repo.createProduct(new Product(
-      0,
-      'X-Ray Scuba Mask',
-      'Watersports',
-      'See what the fish are hiding',
-      49.99,
-      this.repo.products[0].supplier
-    ));
-  }
-
-  createProductAndSupplier() {
-    const supplier = new Supplier(0, 'Rocket Shoe Corp', 'Boston', 'MA');
-    const product = new Product(
-      0,
-      'Rocket-Powered Shoes',
-      'Running',
-      'Set a new record',
-      100,
-      supplier
-    );
-    this.repo.createProductAndSupplier(product, supplier);
-  }
-
-  replaceProduct() {
-    const product = this.repo.products[0];
-    product.name = 'Modified Product';
-    product.category = 'Modified Category';
-    this.repo.replaceProduct(product);
-  }
-
-  replaceSupplier() {
-    const supplier = new Supplier(3, 'Modified Supplier', 'New York', 'NY');
-    this.repo.replaceSupplier(supplier);
-  }
-
-  updateProduct() {
-    const changes = new Map<string, any>();
-    changes.set('name', 'Green Kayak');
-    changes.set('supplier', null);
-    this.repo.updateProduct(1, changes);
-  }
-
-  deleteProduct() {
-    this.repo.deleteProduct(1);
-  }
-
-  deleteSupplier() {
-    this.repo.deleteSupplier(2);
+  clearError() {
+    this.lastError = null;
   }
 }
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ErrorHandler } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ModelModule } from './models/model.module';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from './store/store.module';
 import { ExternalService } from './external.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorHandlerService } from './errorHandler.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +18,15 @@ import { ExternalService } from './external.service';
     FormsModule,
     StoreModule
   ],
-  providers: [ExternalService],
+  providers: [
+    ExternalService,
+    ErrorHandlerService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useExisting: ErrorHandlerService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
